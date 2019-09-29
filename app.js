8a448302a5a2487a8e97ea2e825a9640ecee212a
unknown <anushka.aggarwal@georgebrown.ca>
2019-09-29 17:00:08 -0400

    changes made v0.2

---
+++ app.js
@@ -0,0 +1,38 @@
+function createNode(element)
+{
+    return document.createElement(element);
+}
+
+function append(parent,el)
+{
+    return parent.appendChild(el);
+}
+const ul = document.getElementById("authors");
+
+const url = 'https://randomuser.me/api/?results=10';
+
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data){
+    let authors = data.results;
+
+    return authors.map(function(author){
+        let li = createNode('li'),
+        img = createNode('img'),
+        span = createNode('span');
+
+        img.src = author.picture.medium;
+
+        span.textContent=`${user.name.first} ${user.name.last}`;
+        
+        append(li,img);
+        append(li,span);
+        append(lu,li);
+
+    
+    })
+})
+
+.catch(function(error){
+    console.log(JSON.stringify(error));
+});
